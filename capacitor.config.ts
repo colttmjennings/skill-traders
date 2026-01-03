160993dae2edba95fccc4d98f487a6f8c5c1100e
Coltt Jennings <colttmjennings@gmail.com>
2026-01-02 23:28:32 -0600

    Switch Capacitor to bundled mode (remove server.url)

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,10 +4,6 @@ const config: CapacitorConfig = {
   appId: 'com.skilltraders.app',
   appName: 'Skill Traders',
   webDir: 'out',
-  server: {
-  url: 'https://skill-traders.com/app',
-  cleartext: false
-},
 };
-
 export default config;
+
