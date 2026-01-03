b754bcb77c7b3977680fb2725c1c6184e8b32e35
Coltt Jennings <colttmjennings@gmail.com>
2026-01-02 23:00:57 -0600

    Add clean app landing page at /app

---
+++ android/app/src/main/java/com/skilltraders/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.skilltraders.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,9 +5,9 @@ const config: CapacitorConfig = {
   appName: 'Skill Traders',
   webDir: 'out',
   server: {
-    url: 'https://skill-traders.com',
-    cleartext: false,
-  },
+  url: 'https://skill-traders.com/app',
+  cleartext: false
+},
 };
 
 export default config;
